a19afff7f2a6e52e6bbba3f02689315221bd81f6
lhmisho <lhmisho@gmail.com>
2021-07-04 18:15:26 +0600

    .

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,33 +3,47 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 # Create your models here.
 
 
-class UserAccountManager(BaseUserManager):
-    def create_user(self, name, email, password=None):
-        if not email:
-            raise ValueError("User must have an email address")
-        email = self.normalize_email(email)
-        user = self.model(name=name, email=email)
-        user.set_password(password)
-        user.save()
-        return user
-
-
-class AccountsUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    objects = BaseUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = 'name'
-
-    def get_fullname(self):
-        return self.name
-
-    def get_shortname(self):
-        return self.name
-
-    def __str__(self):
-        return self.name
+# class UserAccountManager(BaseUserManager):
+#     def create_user(self, name, email, password=None):
+#         if not email:
+#             raise ValueError("User must have an email address")
+#         email = self.normalize_email(email)
+#         user = self.model(name=name, email=email)
+#         user.set_password(password)
+#         user.save()
+#         return user
+#
+#     def create_superuser(self, email, name, password):
+#         """
+#         Creates and saves a superuser with the given email and password.
+#         """
+#         user = self.create_user(
+#             name,
+#             email,
+#             password=password,
+#         )
+#         user.staff = True
+#         user.admin = True
+#         user.save(using=self._db)
+#         return user
+#
+#
+# class AccountsUser(AbstractBaseUser, PermissionsMixin):
+#     email = models.EmailField(max_length=255, unique=True)
+#     name = models.CharField(max_length=255)
+#     is_active = models.BooleanField(default=True)
+#     is_staff = models.BooleanField(default=False)
+#
+#     objects = UserAccountManager()
+#
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ('name', )
+#
+#     def get_fullname(self):
+#         return self.name
+#
+#     def get_shortname(self):
+#         return self.name
+#
+#     def __str__(self):
+#         return self.name
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -9,10 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# import djoser.serializers
+#
+# import accounts.serializers
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -57,7 +61,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,10 +80,20 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": 'django_react_auth',
+        "USER": 'postgres',
+        "PASSWORD": 'postgres',
+        "HOST": 'localhost',
+        "PORT": "5432",
     }
 }
 
@@ -121,10 +135,46 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
 
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    # 'USER_CREATE_PASSWORD_RETYPE': True,
+    'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
+    'SEND_CONFIRMATION_EMAIL': True,
+    'SEND_ACTIVATION_EMAIL': True,
+    # 'SET_PASSWORD_RETYPE': True,
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'SERIALIZERS': {
+        'user_create': 'accounts.serializers.UserCreateSerializer',
+        'user': 'accounts.serializers.UserCreateSerializer',
+        'user_delete': 'djoser.serializers.UserDeleteSerializer',
+    }
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'accounts.AccountsUser'
+# AUTH_USER_MODEL = 'accounts.AccountsUser'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'lhmisho@gmail.com'
+EMAIL_HOST_PASSWORD = 'utvmnyhfmbdjjlap'
+EMAIL_PORT = 587
+FROM_EMAIL='no-reply@gmail.com'
+DEFAULT_FROM_EMAIL=EMAIL_HOST_USER
