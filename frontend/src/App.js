7cb530283af3c37941bc144aea118e4610992a26
lhmisho <lhmisho@gmail.com>
2021-07-05 20:54:21 +0600

    wrap apps with store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Layout from './containers/Layout';
 import Home from './containers/Home';
 import Login from './containers/Login'
 import Signup from './containers/Signup'
-import  ResetPassword from './containers/ResetPassword'
+import ResetPassword from './containers/ResetPassword'
 import ResetPasswordConfirm from './containers/ResetPasswordConfirm'
 import Activate from './containers/Activate'
+import {compose} from "redux";
+import store from "./store";
+import {Provider} from "react-redux";
 
 const App = () => {
     return (
-        <Router>
-            <Layout>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/signup" component={Signup} />
-                    <Route exact path="/reset-password" component={ResetPassword} />
-                    <Route exact path="/password/reset/confirm/:uid/:token" component={ResetPasswordConfirm} />
-                    <Route exact path="/activate/:uid/:token" component={Activate} />
-                </Switch>
-            </Layout>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/login" component={Login}/>
+                        <Route exact path="/signup" component={Signup}/>
+                        <Route exact path="/reset-password" component={ResetPassword}/>
+                        <Route exact path="/password/reset/confirm/:uid/:token" component={ResetPasswordConfirm}/>
+                        <Route exact path="/activate/:uid/:token" component={Activate}/>
+                    </Switch>
+                </Layout>
+            </Router>
+        </Provider>
     )
 }
 
---
+++ frontend/src/store.js
@@ -0,0 +1,9 @@
+import {createStore, applyMiddleware, compose} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from './reducers'
+
+const initailState = {};
+const middleware = [thunk];
+const store = createStore(rootReducer, initailState, composeWithDevTools(applyMiddleware(...middleware)));
+export default store;
